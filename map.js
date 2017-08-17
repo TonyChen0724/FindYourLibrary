c5fce95a6587b8fb27ee334b103bd3ab4aee5c03
Tony Chen <tonytonytony0724@gmail.com>
2017-08-17 18:23:49 +1200

    jquery leaflet marker completed
    todo: create a table to present data

--- map.js
+++ map.js
@@ -1,102 +1,127 @@
 /**
  * Created by xichen on 8/12/17.
  */
-//todo: see line 67
+
+//todo: see line 60
+
 var Map = (function() {
 
     var pub = {};
+    var pointsofInterest = {
+        "type": "FeatureCollection",
+        "features":[
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "College of Education"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [ 170.5199, -45.86759]
+                },
+                "id": 1 },
+        ] };
 
-    var mymap = L.map('mapid').setView([-45.8744, 170.5029], 14);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
-
-
-    // var pointsofInterest = {
-    //     "type": "FeatureCollection",
-    //     "features":[
-    //         {
-    //             "type": "Feature",
-    //             "properties": {
-    //                 "description": "College of Education"
-    //             },
-    //             "geometry": {
-    //                 "type": "Point",
-    //                 "coordinates": [ 170.5199, -45.86759]
-    //             },
-    //             "id": 1 },
-    //         {
-    //             "type": "Feature",
-    //             "properties": {
-    //                 "description": "HCI lab"
-    //             },
-    //             "geometry": {
-    //                 "type": "Point",
-    //                 "coordinates": [170.51528, -45.86788]
-    //             },
-    //             "id": 2 }
-    //     ] };
-    //
-    //
-    // var obj = {"type": "Feature",
-    //     "properties": {
-    //         "description": "HCI lab"
+    //{"type":"FeatureCollection",
+    // "features":[
+    // {
+    //      "type":"Feature",
+    //      "properties":{
+    //          "description":"145 Union St East"
     //     },
-    //     "geometry": {
-    //         "type": "Point",
-    //         "coordinates": [170.51600, -45.86800]
-    //     },
-    //     "id": 2 };
-    //pointsofInterest.features[0].coordinates = [170.81528, -45.8749];
+    //      "geometry":{
+    // "type":"Point","coordinates":[-45,170]},
+    // "id":1},
+    // {
+    //      "type":"Feature",
+    //      "properties": {
+    //          "description":"597 Castle St"},
+    //      "geometry":{"type":"Point","coordinates":[-45,170]},"id":2},
+    // {"type":"Feature","properties":{"description":"33 Baldwin St"},"geometry":{"type":"Point","coordinates":
+    // [-45,170]},"id":3}]}
+
+
 
 
     var pointsofLibrary = {};
+    pointsofLibrary.type = "FeatureCollection";
     pointsofLibrary.features = [];
     /* this function is for converting xml file to GeoJSON file. */
     function appendToGeo() {
-        var fileName = "./libraries.xml";
-
+        var fileName = "./new.xml";
         var success = $.ajax({
             type: "GET",
             url: fileName,
             cache: false,
             success: function (data) {
-                converting(data); // this statement is not activated, using classicalCinima to find out why?
+                converting(data);
+                console.log(JSON.stringify(pointsofLibrary));
+                //popup.setContent("<pre" + JSON.stringify(geojson, null, 2) + "</pre>");
+                //L.geoJSON(pointsofLibrary).addTo(mymap); // geoJSON is not working, find out why?
+
+                var mymap = L.map('mapid').setView([-45.8744, 170.5029], 14);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                    maxZoom: 18,
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                    id: 'mapbox.streets'
+                }).addTo(mymap);
+                var pointsofLibraryLayer = L.geoJSON(pointsofLibrary, {
+                    onEachFeature: onEachFeatureImpl
+                }).addTo(mymap);
+            },
+            error:  function(xhr, status, error) {
+                var err = eval("(" + xhr.responseText + ")");
+                alert(err.Message);
             }
+
         });
     }
 
-    function converting(data) {
+    function onEachFeatureImpl(feature, layer) {
+        // var popupContent = "<b>Point of Interest</b> <br/>" +
+        //     feature.properties.description;
+        var i = 0;
+        var popupContent = "<pre>" + JSON.stringify(pointsofLibrary, null, 2) + "</pre>";
+        layer.bindPopup(popupContent);
+    }
 
-        var addresses = $(data).find("address");
+    function converting(data) {
+        var addresses = $(data).find("library");
         if (addresses.length == 0) {
             alert("no library information");
         }
         else {
-            for (i = 0; i < addresses.length; i++) {
+            $(data).find("library").each(function() {
                 var object = {};
+                object.type = "Feature";
                 object.properties = {};
                 object.geometry = {};
-                object.properties.description = $(data).find("address")[i].textContent;
                 object.geometry.type = "Point";
+                var address = $(this).find("address")[0].textContent;
+                var lat = $(this).find("lat")[0].textContent;
+                var lng = $(this).find("lng")[0].textContent;
+                var id = $(this).find("id")[0].textContent;
+                object.properties.description = address;
                 object.geometry.coordinates = [];
-                object.geometry.coordinates.push($(data).find("lat")[i].textContent);
-                object.geometry.coordinates.push($(data).find("lng")[i].textContent);
-                object.id = i + 1;
+                object.geometry.coordinates.push(parseFloat(lng));
+                object.geometry.coordinates.push(parseFloat(lat));
+                object.id = parseInt(id);
                 pointsofLibrary.features.push(object);
-            }
+
+
+            })
+
+
+
+
         }
     }
 
 
     pub.setup = function() {
         appendToGeo();
-        alert(JSON.stringify(pointsofLibrary));
-        L.geoJSON(pointsofLibrary).addTo(mymap);
     };
     return pub;
 }());
