14a3bab11b5bd357d4a14c8fdce6917dc439dac3
Tony Chen <tonytonytony0724@gmail.com>
2017-08-20 20:54:24 +1200

    todo: next create an administrative webpage to change those information

--- map.js
+++ map.js
@@ -4,21 +4,9 @@
 
 //todo: the pop up information is still not right, fix that.
 var Map = (function() {
+    var adressSpecifier = []; // I use this addressSpecifier for the convenience of adding an onclick to each address
     var pub = {};
-    var pointsofInterest = {
-        "type": "FeatureCollection",
-        "features":[
-            {
-                "type": "Feature",
-                "properties": {
-                    "description": "College of Education"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [ 170.5199, -45.86759]
-                },
-                "id": 1 },
-        ] };
+
 
     //{"type":"FeatureCollection",
     // "features":[
@@ -41,6 +29,7 @@ var Map = (function() {
 
 
     var statement = "<tr> <th>Address</th> <th>Number of books present</th> <th>Capacity of library</th></tr>";
+    var bookstatement = "";
     var pointsofLibrary = {};
     pointsofLibrary.type = "FeatureCollection";
     pointsofLibrary.features = [];
@@ -92,15 +81,20 @@ var Map = (function() {
         }
         else {
             $(data).find("library").each(function() {
+                var address = $(this).find("address")[0].textContent;
+                var lat = $(this).find("lat")[0].textContent;
+                var lng = $(this).find("lng")[0].textContent;
+                var id = $(this).find("id")[0].textContent;
+                // soak the address as id
+                var tableid = address.replace(/\s+/g, '');
+                tableid = tableid.replace(/\d+/g, '');
+
+                bookstatement += "<table id='" + tableid + "' style='width:100%' border='1px solid black'> <tr><th>title</th> <th>author</th> <th>year</th></tr>";
                 var object = {};
                 object.type = "Feature";
                 object.properties = {};
                 object.geometry = {};
                 object.geometry.type = "Point";
-                var address = $(this).find("address")[0].textContent;
-                var lat = $(this).find("lat")[0].textContent;
-                var lng = $(this).find("lng")[0].textContent;
-                var id = $(this).find("id")[0].textContent;
                 object.properties.description = address;
                 object.geometry.coordinates = [];
                 object.geometry.coordinates.push(parseFloat(lng));
@@ -108,17 +102,44 @@ var Map = (function() {
                 object.id = parseInt(id);
                 pointsofLibrary.features.push(object);
 
+
+                var booknum = $(this).find("title").length;
+                for (var i = 0; i < booknum; i++) {
+                    var titles = $(this).find("title")[i].textContent;
+                    var authors = $(this).find("author")[i].textContent;
+                    var years = $(this).find("year")[i].textContent;
+
+                    bookstatement += "<tr><td>" + titles + "</td><td>" + authors +
+                        "</td><td>" + years + "</td></tr>";
+                }
+
+
                 object.properties.booknumber = $(this).find("book").length;
                 object.properties.capacity = $(this).find("capacity")[0].textContent;
-                statement += "<tr><td>" + address + "</td><td>" + object.properties.booknumber +
+                statement += "<tr><td id='" + tableid + "0'>" + address + "</td><td>" + object.properties.booknumber +
                     "</td><td>" + object.properties.capacity + "</td></tr>";
 
+                adressSpecifier.push(tableid + "0");
 
 
 
             })
-            console.log(statement);
+
             $("#libraryinfo").html(statement);
+            $("#Bookinfo").html(bookstatement);
+            $("#Bookinfo").children().hide(); // I hide them all
+
+
+            console.log(adressSpecifier);
+            for (f = 0; f < adressSpecifier.length; f++) {
+                var sel = "#" + adressSpecifier[f];
+
+                $(sel).click (function() {
+                    var tablespecifier = this.id.replace(/\d+/g, '');
+                    $("#" + tablespecifier).toggle();
+                });
+                console.log(sel);
+            }
 
 
 
@@ -129,6 +150,8 @@ var Map = (function() {
 
     pub.setup = function() {
         appendToGeo();
+        // I must find a way to hide all this book info table first
+
     };
     return pub;
 }());
